c32f00e66c2cfc710184720847881f0ce51441ae
Marcos Guilherme <55901750+CZERNOB0G@users.noreply.github.com>
2021-06-09 00:54:18 -0300

    Update backup-sql-lxc.sh

--- backup-sql-lxc.sh
+++ backup-sql-lxc.sh
@@ -2,29 +2,36 @@
 backup="/var/backup"
 pass=`grep PASS /usr/local/sbin/mysqldump_backup.rb | head -1 | cut -d\' -f2`;
 mount $backup
-#$1 = banco
-if [ -z `find $backup -iname "$1.sql.gz"` > /dev/null 2>&1 ];
+banco = "$1";
+if [ -z "$banco" ];
+    then
+        echo "============================================"
+        echo "= Parametro do banco nulo, informe o banco ="
+        echo "============================================"
+        umount $backup
+        exit;
+fi
+if [ -z `find $backup -iname "$banco.sql.gz"` > /dev/null 2>&1 ];
     then
         echo "================================"
         echo "= Não existe backup deste banco="
         echo "================================"
-        umount /var/backup
+        umount $backup
         exit;
 fi
-corrige_unicode_banco=`echo "$1" | tr -s "[:punct:]" "*"` > /dev/null 2>&1;
-if [ -z `find /var/lib/lxc/mysql/ -iname "$corrige_unicode_banco"` > /dev/null 2>&1 ];
+if [ -z `find /var/lib/mysql/ -iname "$banco"` > /dev/null 2>&1 ];
     then
         echo "========================="
         echo "= Esse banco não existe ="
         echo "========================="
-        umount /var/backup
+        umount $backup
         exit;
 fi
 echo "========================================"
 echo "= Escolha a data que deseja restaurar: ="
 echo "========================================"
 data_find=()
-for i in `find $backup -iname "$1.sql.gz" | sort | cut -d'/' -f5`; 
+for i in `find $backup -iname "$banco.sql.gz" | sort | cut -d'/' -f5`; 
     do 
     	let inc++;
     	data_find[$inc]=$i;
@@ -42,13 +49,11 @@ if [ -z ${data_find[$id_data_solicitada]} ];
 fi
 data_solicitada="${data_find[$id_data_solicitada]}"
 echo "------------->Descompactando o backup: $data_solicitada..."
-zcat /var/backup/mysqldump/$data_solicitada/$1.sql.gz > /var/backup/$1.sql;
-conteiner=`find /var/lib/lxc/mysql -iname "$corrige_unicode_banco" | awk -F'/' '{print $6}' | head -n1`;
-echo "------------->Definindo conteiner: $conteiner";
+zcat $backup/mysqldump/$data_solicitada/$banco.sql.gz > /var/backup/$banco.sql;
 echo "------------->Restaurando..."
-lxc-attach -n $conteiner -- mysql -p$pass $1 < /var/backup/$1.sql;
+mysql -p$pass $banco < $backup/$banco.sql;
 echo "------------->Desmontando partição de backup..."
-rm /var/backup/$1.sql
+rm $backup/$banco.sql
 umount $backup
 echo "------------->Backup finalizado!"
 exit;
